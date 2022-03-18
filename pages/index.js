8eddce276f41918afc8934314ca14a5abb46a9f3
Ben <veliebm@gmail.com>
2022-03-18 15:29:42 -0400

    Consolidate Heads / Nav into _app

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
-import Head from "next/head";
-import Nav from "../components/nav/nav";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Home</title>
-        <meta name="description" content="A site for all things Ben." />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-
-      <Nav />
-
       <main className={styles.main}>
         <h1 className={styles.title}>Benjamin Velie</h1>
         <p className={styles.description}>I like software and I like brains.</p>
--- pages/resume.js
+++ pages/resume.js
@@ -1,18 +1,8 @@
-import Head from "next/head";
-import Nav from "../components/nav/nav.js";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Resume</title>
-        <meta name="description" content="A site for all things Ben." />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-
-      <Nav />
-
       <main className={styles.main}>
         <h1 className={styles.title}>Resume</h1>
         <p className={styles.description}>
